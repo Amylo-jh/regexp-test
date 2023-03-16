5b42d6e61dd8ba368d7475093f28c8aeea3ae89a
Amylo <amylose1110@gmail.com>
2023-03-16 19:50:20 +0900

    Part 4 - Ch 3 - 06

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 let str = `
 010-1234-5678
-thesecon@gmail.com
+thesecon@gmail.com.
 https://www.omdbapi.com/?apikey=1451733a&s=frozen
 The quick brown fox jumps over the lazy dog.
 abbccdddd
+http://localhost:1234
+동해물과백두산ㅇ_마르고닳 도롪 ㅇㅇㅇ ㅏㅏㅏ
 `
 
-const regexp = /fox/gi
-console.log(regexp.test(str))
-str = str.replace(regexp, 'AAAA')
-console.log(str)
+console.log(
+    str.match(/(?<=@).{1,}/g)
+)
